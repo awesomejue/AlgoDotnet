a7a2d36142e3e982c7a40654c0007198ce16b299
jueying <jueying2014@foxmail.com>
2019-05-20 23:57:25 +0800

    init

---
+++ AlgoPlayground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgoPlayground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
