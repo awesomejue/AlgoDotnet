2b33c2b1bee62768ec02bfa154a6fa80015b1c89
ligang <gang.li@welines.cn>
2019-07-01 11:06:42 +0800

    refine quick sort

--- AlgoPlayground/QuickSort.cs
+++ AlgoPlayground/QuickSort.cs
@@ -3,56 +3,58 @@ using System.Collections.Generic;
 
 namespace AlgoPlayground
 {
-  public class QuickSort : AlgoSortBase
-  {
-
-    public Random random { get; private set; }
-    public QuickSort(List<IComparable> list) : base(list)
+    public class QuickSort : AlgoSortBase
     {
-      random = new Random();
-    }
 
-    override public void Sort()
-    {
-      if (this.List == null || this.List.Count == 0)
+      public Random random { get; private set; }
+      public QuickSort(List<IComparable> list) : base(list)
       {
-        return;
-      }
-      quickSort(this.List, 0, this.List.Count - 1);
-    }
-
-    void quickSort(List<IComparable> list, int left, int right)
-    {
-      if (left >= right)
-      {
-        return;
+        random = new Random();
       }
 
-      int pivot = partition(list, left, right, random.Next(left, right));
-      quickSort(list, left, pivot - 1);
-      quickSort(list, pivot + 1, right);
-    }
-
-    int partition(List<IComparable> list, int left, int right, int pivot)
-    {
-      // Console.WriteLine($"{String.Concat(List)} | left: {left}, right: {right}, pivot: {list[pivot]} at {pivot}");
-
-      list.Swap(right, pivot);
-
-      int rover = left;
-      for (int i = left; i < right; i++)
+      override public void Sort()
       {
-        if (list[i].Lessthan(list[right]))
+        if (this.List == null || this.List.Count == 0)
         {
-          // Console.WriteLine($"{i}, {rover}");
-          list.Swap(i, rover++);
-          // Console.WriteLine($"{String.Concat(List)}");
+          return;
         }
+        quickSort(this.List, 0, this.List.Count - 1);
       }
 
-      list.Swap(rover, right);
+      void quickSort(List<IComparable> list, int left, int right)
+      {
+        if (left >= right)
+        {
+          return;
+        }
 
-      return rover;
+        int mid = partition(list, left, right);
+        quickSort(list, left, mid - 1);
+        quickSort(list, mid + 1, right);
+      }
+
+      int partition(List<IComparable> list, int left, int right)
+      {
+        // Console.WriteLine($"{String.Concat(List)} | left: {left}, right: {right}, pivot: {list[pivot]} at {pivot}");
+
+        int pivot = random.Next(left, right);
+
+        list.Swap(right, pivot);
+
+        int rover = left;
+        for (int i = left; i < right; i++)
+        {
+          if (list[i].Lessthan(list[right]))
+          {
+            // Console.WriteLine($"{i}, {rover}");
+            list.Swap(i, rover++);
+            // Console.WriteLine($"{String.Concat(List)}");
+          }
+        }
+
+        list.Swap(rover, right);
+
+        return rover;
+      }
     }
   }
-}
